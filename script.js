cc8acfad6b69cfa74089e29c45dd3a4c7835674d
realotakuboi <sohamseth2005@gmail.com>
2022-07-20 18:29:43 +0530

    javascript

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ const selectBox = document.querySelector(".select-box");
 const selectXBtn = selectBox.querySelector(".playerX");
 const selectOBtn = selectBox.querySelector(".playerO");
 const playBoard = document.querySelector(".play-board");
+const boxes = document.querySelectorAll("section span");
 
 
 window.onload = () => {
-    selectBox.addEventListener("click", () => {
+
+    for(let i = 0; i < boxes.length; i++){
+        boxes[i].setAttribute("onclick","clickedBox(this)");
+    }
+
+
+    selectXBtn.addEventListener("click", () => {
+        selectBox.classList.add("hide");
+        playBoard.classList.add("show");
+    });
+
+    selectOBtn.addEventListener("click", () => {
         selectBox.classList.add("hide");
         playBoard.classList.add("show");
     });
