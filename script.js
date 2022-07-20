a431aac433370ba5b152e909dca68d2a5bb10199
realotakuboi <sohamseth2005@gmail.com>
2022-07-20 18:20:30 +0530

    minor CSS Tweaks

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 const selectBox = document.querySelector(".select-box");
 const selectXBtn = selectBox.querySelector(".playerX");
 const selectOBtn = selectBox.querySelector(".playerO");
+const playBoard = document.querySelector(".play-board");
+
 
 window.onload = () => {
     selectBox.addEventListener("click", () => {
-        selectBox.classList.add("hide")
+        selectBox.classList.add("hide");
+        playBoard.classList.add("show");
     });
 };
