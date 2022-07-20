860607fcd7b6dc905ce18232dcc5050d589be39a
realotakuboi <sohamseth2005@gmail.com>
2022-07-20 19:24:32 +0530

    Something done

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const selectXBtn = selectBox.querySelector(".playerX");
 const selectOBtn = selectBox.querySelector(".playerO");
 const playBoard = document.querySelector(".play-board");
 const boxes = document.querySelectorAll("section span");
+const players = document.querySelector(".players");
 
 
 window.onload = () => {
@@ -22,5 +23,15 @@ window.onload = () => {
     selectOBtn.addEventListener("click", () => {
         selectBox.classList.add("hide");
         playBoard.classList.add("show");
+        players.setAttribute("class", "players active")
     });
 };
+
+let Xicon = "fa-solid fa-xmark";
+let Oicon = "fa-solid f-o";
+
+function clickedBox(element){
+    if(){
+
+    }
+}
